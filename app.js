95822df2f28cc1796e713e2fe1ec7a4ebae7c664
paulinavita <paulinadavita@gmail.com>
2019-02-28 16:35:58 +0700

    append bulk to db done w.o session

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const app = express()
 const routes = require("./routes")
-var session = require('express-session')
+const routesDrugs = require("./routes/drugs/drugs")
+const session = require('express-session')
+const routesSymptoms = require("./routes/symptoms/symptoms")
 
 app.set("view engine", "ejs")
+app.use(express.static('css'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+// app.use(session)
 
 app.use('/', routes)
+// app.use('/session' ) {}
+app.use('/check', routesSymptoms )
 
 app.listen(3000);
+console.log('listening..')
