53212d6c01b45a8d2ede8601a65665c107787f2b
yogapermana76 <permana76.yp@gmail.com>
2019-02-28 16:46:47 +0700

    revisi login register

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express()
 const routes = require("./routes")
-var session = require('express-session')
 
+const port = 3000
+
+app.use(express.static('public'))
 app.set("view engine", "ejs")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', routes)
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`running in port ${port}`)
+});
